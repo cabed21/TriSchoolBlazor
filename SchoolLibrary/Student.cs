293226cfc7fe5dc86cc6e2361536d108205bf37d
Chris Barcia <cabed21@gmail.com>
2019-06-18 22:33:28 -0400

    Added Students Compenent to SchoolClient

---
+++ SchoolLibrary/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string School { get; set; }
+    }
+}
